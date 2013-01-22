329802f8742cc9917f3c5188ca77768f747c6300
wrm <ptmucha@gmail.com>
2013-01-22 11:43:10 +0100

    added about dialog

--- code/pages/index.py
+++ code/pages/index.py
@@ -9,22 +9,24 @@ class PageController:
     @cherrypy.tools.template(template='status.html')
     def index(self):
         return  {"cpu":{'usage':'65',
-                        'processes':'30'}}
+                        'processes':'30'},
+                 "user":cherrypy.request.login
+                }
     
     @cherrypy.expose
     @cherrypy.tools.template(template='gpio.html')
     def gpio(self):
-        return  {}
+        return  {"user":cherrypy.request.login}
     
     
     @cherrypy.expose
     @cherrypy.tools.template(template='bash.html')
     def bash(self):
-        return  {}
+        return  {"user":cherrypy.request.login}
         
         
     @cherrypy.expose
     @cherrypy.tools.template(template='house.html')
     def house(self):
-        return  {}
+        return  {"user":cherrypy.request.login}
     
