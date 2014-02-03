839df9062f70df5e5144f21612fe8b1e3800e796
Peter <ptmucha@gmail.com>
2014-02-03 11:41:30 -0500

    added elro class

--- code/pages/plug.py
+++ code/pages/plug.py
@@ -1,5 +1,5 @@
 from util.jsonify import jsonify
-
+from util.elro import Elro
 
 
 class Plug:
@@ -10,6 +10,7 @@ class Plug:
           1,
           0
       ]
+      self.elro = Elro();
     
     
     @jsonify
@@ -21,5 +22,6 @@ class Plug:
         id = int(id);
         newState= (self.states[id] -1) * -1;
         print("switching plug " + str(id+1) + " to state " + str(newState));
+        self.elro.switch(id, newState);
         self.states[id] = newState;
         return newState
---
+++ code/util/elro.py
@@ -0,0 +1,20 @@
+
+class Elro:
+     
+    def __init__(self):
+      
+    
+    
+   
+    def switch(self, id, on):
+        if (on)
+            return self.switchOn(id);
+        else
+            return self.switchOff(id);
+        
+    def switchOn(self, id):
+        return true;
+    
+    def switchOff(self, id):
+        return true;
+    
