d1b912a54a791dc874000507d4556828eb96e559
ptmucha <ptmucha@gmail.com>
2014-02-15 11:58:30 +0100

    bla

--- files/js/main.js
+++ files/js/main.js
@@ -2,5 +2,12 @@ require(["jquery"], function($) {
     //the jquery.alpha.js and jquery.beta.js plugins have been loaded.
     $(function() {
         $('body').alpha().beta();
+
+        
+
+
+
+
+
     });
 });
